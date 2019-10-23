61d8a2758cc2d5e90900d59d87a1523fb3b6f81b
EC2 Default User <ec2-user@ip-172-31-93-210.ec2.internal>
2019-10-23 18:50:49 +0000

    Add project files

---
+++ README.md
@@ -0,0 +1,60 @@
+<h1 align="center">
+Milestone Project Two <br>Simon Memory Game<br> Interactive Frontend Module 
+</h1>
+
+
+<div> 
+The Simon memory game uses four primary colours accompanied by four musical tones.  It's a familiar and accessible game which is playable by all ages and abilities.  Children of the eighties may remember the physical Simon devices.  This project aims to replicate some of the functionality of the physical Simon game through the use of Javascript, HTML and CSS.    
+</div>
+
+
+## UX
+
+## Project Aim
+
+This memory game should be interactive, easy to use, engaging, challenging and responsive on different devices.
+
+### User Stories
+
+- I played the actual Simon game and would like to try it out online.
+- The site seems simple and it looks quite like the Simon game.
+- There are no intrusive sounds playing when I visit the site initially.
+- I can see the play button to start the game.  I can see a strict option. I can see a score counter.
+- If I want to know more I can see an information button on How to Play the game, below the game board.
+- The game tones are not unpleasant and some of them are reminiscent of the original game.
+- I can turn strict mode off if I want to practice.
+
+### User Experience
+
+- Primary colours are used in keeping with the original game, which users might recognise.
+- The Simon name is visible at the top of the page, and also in the centre of the game board.
+- The colourful board stands out against a dark background.
+- The user sees a dash on the counter prior to engaging with the game.  When they start they should see this counter increment     by 1 on each successful turn. This is common and should be recognisable.
+- The green start button has a universally recognised "play" symbol taken from Font Awesome
+- Strict mode is automatically activated to allow users to start the game immediately
+- There is a "How to Play" button below the game board if the user needs further information on how the game works.  In the "How to Play" section, it is explained that Strict mode can be turned off for practice runs.
+- A losing game modal and sound is implemented to signify to players in strict mode that the game is over
+- A winning game modal and sound is implemented to signify to players in strict mode that they have won the game
+- Users should not need to reload the page or to press the back button as there are clear options available
+
+### Wireframes
+
+Wireframes of the inital concept and design for desktop, tablet and smartphone were created using [Balsamiq](https://balsamiq.com/).
+
+- [Desktop](wireframes/desktop.png) 
+- [Tablet](wireframes/tablet.png) 
+- [Smartphone](wireframes/smartphone.png) 
+
+#### Styling
+
+    
+- #### Backgrounds
+
+    The background image on the main site, the information and lose modals, and the win modal are royalty free images from [Unsplash](https://unsplash.com/).
+    
+
+
+## Features
+
+### Existing Features
+
---
+++ assets/js/index.js
@@ -0,0 +1,339 @@
+// Variables
+
+let strictMode = true;
+let pattern = [];
+let playerPattern = [];
+let turn, playerCount, computerCount, playTimeout, playInterval;
+
+// JQuery variables caching modals, coloured blocks, buttons and score counter
+
+const block = document.getElementsByClassName('block');
+const yellowBlock = document.getElementById("1");
+const blueBlock = document.getElementById("2");
+const redBlock = document.getElementById("3");
+const greenBlock = document.getElementById("4");
+const playButton = document.getElementById("start");
+const strictSlider = document.getElementById("strict");
+const scoreBoard = document.getElementById("score-counter");
+const scrollModalArrow = document.getElementById('modal-scroll');
+const startModal = document.getElementById("start-modal-button");
+const startWinModal = document.getElementById("start-modal-win-button");
+const loseModalDisplay = document.getElementById("lose-modal-display");
+const winModalDisplay = document.getElementById("win-modal-display");
+
+// Code inside the function below will wait until an event signals that the Document Object Model is fully created
+
+$(document).ready(function() {
+    
+// Function to start new game on click
+
+    $(playButton).click(function() {
+        beginGame();
+    });
+
+    $(startModal).on("click", function() {
+        beginGame();
+    });
+
+    $(startWinModal).on("click", function() {
+        beginGame();
+    });
+    
+// Function to scroll modal on click
+
+    $(scrollModalArrow).on('click', function(e) {
+        var linkHref = $(this).attr('href');
+        e.preventDefault();
+        $('.modal-body').animate({
+            scrollTop: $(linkHref).offset().top
+        }, 1500);
+    });
+
+// Function checks if strict slider true/false when clicked
+// If true, return to initial new game status
+
+    $(strictSlider).on("click", function() {
+        if (strictSlider.checked == true) {
+            strictMode = true;
+            turn = 1;
+            $(".block").addClass('disabled');
+            clearInterval(playInterval);
+            if ($(scoreBoard).text() == "-") {
+                $(scoreBoard).text("-");
+            }
+            else {
+                $(scoreBoard).text("0");
+            }
+            setTimeout(function() {
+                removeLightOnAllBlocks();
+            }, 500);
+        }
+        else {
+            strictMode = false;
+        }
+    });
+    
+// Block click events using the pattern array and block id numbers with respective audio and light
+   
+    $(block).click(function() {
+        clearTimeout(playTimeout);
+        removeLightOnAllBlocks();
+        let blockId = $(this).attr('id');
+        if (blockId == 1) {
+            yellowLightWithAudio();
+            playerPattern.push(parseInt(blockId));
+        }
+        if (blockId == 2) {
+            blueLightWithAudio();
+            playerPattern.push(parseInt(blockId));
+        }
+        if (blockId == 3) {
+            redLightWithAudio();
+            playerPattern.push(parseInt(blockId));
+        }
+        if (blockId == 4) {
+            greenLightWithAudio();
+            playerPattern.push(parseInt(blockId));
+        }
+        checking();
+    });
+});
+
+// New game function restores default settings
+
+function beginGame() {
+    clearInterval(playInterval);
+    removeLightOnAllBlocks();
+    $(scoreBoard).text('0');
+    pattern = [];
+    turn = 0;
+    $(".block").addClass('disabled');
+    randomNumber();
+    gamePlay();
+}
+
+// Select a number and place it into the pattern array
+
+function randomNumber() {
+    randomNum = Math.ceil(Math.random() * 4);
+    pattern.push(randomNum);
+    console.log(pattern);
+}
+
+/* 
+The Function is run when it's the computers turn to generate a pattern.
+The turn will increment the turn by 1, set the player count and the computer count back to 0
+and set the player pattern back to an empty string.
+the player interval of 800 milliseconds is then run which holds a switch statement that will
+check each of the numbers inside of the pattern array, it will produce a sound and light depending on which number
+has been found in the switch statement & break after each avoiding each light and sound playing on the previous
+pad that's been selected. The number of times this will be run is the number value of the computer count.
+When the array 'pattern' length matches the computer count it will then stop the play interval and allow
+the player to start clicking.
+The computer count is then incremented by 1.
+*/
+
+function gamePlay() {
+    turn++;
+    playerCount = 0;
+    computerCount = 0;
+    playerPattern = [];
+    playInterval = setInterval(function() {
+        switch (pattern[computerCount]) {
+            case 1:
+                yellowLightWithAudio();
+                break;
+            case 2:
+                blueLightWithAudio();
+                break;
+            case 3:
+                redLightWithAudio();
+                break;
+            case 4:
+                greenLightWithAudio();
+                break;
+            default:
+                break;
+        }
+        if (pattern.length === computerCount) {
+            clearInterval(playInterval);
+            $(".block").removeClass('disabled');
+        }
+        computerCount++;
+    }, 800);
+}
+
+// Functions which give the light and audio to the blocks
+
+function yellowLightWithAudio() {
+    $(yellowBlock).addClass('bright-yellow');
+    playerTimeout();
+    playAudio('yellow');
+}
+
+function blueLightWithAudio() {
+    $(blueBlock).addClass('bright-blue');
+    playerTimeout();
+    playAudio('blue');
+}
+
+function redLightWithAudio() {
+    $(redBlock).addClass('bright-red');
+    playerTimeout();
+    playAudio('red');
+}
+
+function greenLightWithAudio() {
+    $(greenBlock).addClass('bright-green');
+    playerTimeout();
+    playAudio('green');
+}
+
+/* 
+This function where the light of all of the pads are being removed, this has a timeout function that will
+remove all lights after 400 milliseconds.
+*/
+
+function playerTimeout() {
+    playTimeout = setTimeout(function() {
+        removeLightOnAllBlocks();
+    }, 400);
+}
+
+/*
+This function where all of the sounds are generated. This takes an argument of 'audioPlayed' and wherever the function
+is called, it will take the argument of whatever sound needs to be implemented after 'sound-'.
+*/
+
+function playAudio(audioPlayed) {
+    let sound = $(`#sound-${audioPlayed}`)[0];
+    sound.currentTime = 0;
+    sound.play();
+}
+
+/* 
+This function returns all of the colours back to their original state from being flashed by removing
+the light class name through jQuery. 
+*/
+
+function removeLightOnAllBlocks() {
+    $(yellowBlock).removeClass("bright-yellow");
+    $(blueBlock).removeClass("bright-blue");
+    $(redBlock).removeClass("bright-red");
+    $(greenBlock).removeClass("bright-green");
+}
+
+// This function will flash all of the colours at the same time using jQuery.
+
+function addLightsToAllBlocks() {
+    $(yellowBlock).addClass("bright-yellow");
+    $(blueBlock).addClass("bright-blue");
+    $(redBlock).addClass("bright-red");
+    $(greenBlock).addClass("bright-green");
+}
+
+/* 
+This function is to order a game over modal to appear with the final score when the game is lost.
+This will show the modal & show the final score using jQuery.
+*/
+
+function displayModal() {
+    $('#loseModal').modal('show');
+    $(loseModalDisplay).text(turn);
+}
+
+/*
+This is the function which does the entire checking of the game, this will increment the player count and has two variables assigned
+which checks if the player pattern and computer pattern match and player pattern and computer pattern don't match.
+This will then apply an if statement to check each of the possible scenarios during the game and implement an
+outcome accordingly.
+*/
+
+function checking() {
+    playerCount++;
+    let playerAndCompSeqMatch = playerPattern[playerCount - 1] === pattern[playerCount - 1];
+    let playerAndCompSeqDontMatch = playerPattern[playerCount - 1] !== pattern[playerCount - 1];
+    /* 
+    This if statement checks if the player has got to the 20th play count, strict mode is true and
+    the player pattern array matches the computer pattern array. if so then the play interval is stopped,
+    the pads are disabled and the winning function is run.
+    */
+    if (playerCount === 2 && strictMode && playerAndCompSeqMatch) {
+        clearInterval(playInterval);
+        $(".block").addClass('disabled');
+        winGame();
+    }
+    /* 
+    This statement checks to see if the player pattern array matches the computer pattern array. If it does,
+    then a new random number is pushed into the computer array, pads are displayed, number display is updated
+    to a new score and gameplay is then run on a set timeout.
+    */
+    else if (playerAndCompSeqMatch) {
+        if (playerPattern.length === turn) {
+            randomNumber();
+            $(".block").addClass('disabled');
+            $(scoreBoard).text(playerCount);
+            setTimeout(gamePlay, 500);
+        }
+    }
+    /* 
+    This statement checks to see if the player pattern array doesn't match with the computer pattern array and strict
+    mode is not active, it will disable the pads, switch the turn back to the previous turn and light all pads,
+    this will then produce a losing sound along with "Try Again" displaying on the number display with a set timeout
+    function to split each word, it will then remove all of the colours on the pads after a set timeout of 400 milliseconds,
+    display the current score after a set timeout of 600 seconds and re-run the previous pattern again with the set timeout function.
+    */
+    else if (playerAndCompSeqDontMatch && !strictMode) {
+        $(".block").addClass('disabled');
+        turn--;
+        playAudio('lost');
+        $(scoreBoard).text('Oops!');
+        addLightsToAllBlocks();
+        setTimeout(function() {
+            removeLightOnAllBlocks();
+            $(scoreBoard).text('Nope!');
+            setTimeout(function() {
+                $(scoreBoard).text(turn);
+                setTimeout(gamePlay, 500);
+            }, 600);
+        }, 600);
+    }
+    /* 
+    With the only other option being to have the player pattern array not matching the computer pattern array and
+    strict mode to be true, an else statement is then implemented to say the game has been lost in strict mode.
+    This will disable the pads, produce a losing sound, display "lost" in the number display and make all pad flash.
+    the flash is removed after a set timeout of 400 milliseconds. Then after 600 milliseconds, it will display a losing,
+    modal informing the player of their score and display the score in the number display.
+    */
+    else {
+        $(".block").addClass('disabled');
+        playAudio('lost');
+        $(scoreBoard).text('Lose');
+        addLightsToAllBlocks();
+        setTimeout(function() {
+            removeLightOnAllBlocks();
+            setTimeout(function() {
+                $(scoreBoard).text(turn);
+                displayModal();
+            }, 600);
+        }, 400);
+    }
+}
+
+/* 
+Function generated when the player wins the game. This will change the number display to say "WIN!",
+flash all pads and stop the player timeout to avoid the player timeout to carry on running and
+interrupting the lights generated by the pads all flashing. This will then set a timeout of 1400 milliseconds
+to generate a winning noise and display the winning modal with the maximum score.
+*/
+
+function winGame() {
+    $(scoreBoard).text("VICTORY!");
+    clearTimeout(playerTimeout);
+    addLightsToAllBlocks();
+    setTimeout(function() {
+        $(winModalDisplay).text(turn);
+        $('#winModal').modal('show');
+        playAudio('win');
+    }, 1400);
+}
